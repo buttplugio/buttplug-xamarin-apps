be4179c9d48076d3ebb06c7d5e48669b7e33f630
NZSmartie <nzsmartie@gmail.com>
2018-02-18 09:46:03 +1300

    Begin work on Bluetooth manager

---
+++ src/Buttplug.Server.XamarinBluetoothManager/XamarinBluetoothDevice.cs
@@ -0,0 +1,31 @@
+﻿using Buttplug.Server.Bluetooth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Buttplug.Core;
+using System.Threading.Tasks;
+
+namespace Buttplug.Server.XamarinBluetoothManager
+{
+    internal class XamarinBluetoothDevice : IBluetoothDeviceInterface
+    {
+        public string Name => throw new NotImplementedException();
+
+        public event EventHandler DeviceRemoved;
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong GetAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ButtplugMessage> WriteValue(uint aMsgId, Guid aCharacteristicIndex, byte[] aValue, bool aWriteWithResponse = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Buttplug.Server.XamarinBluetoothManager/XamarinBluetoothManager.cs
@@ -0,0 +1,32 @@
+﻿using Buttplug.Server.Bluetooth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Buttplug.Core;
+
+namespace Buttplug.Server.XamarinBluetoothManager
+{
+    public class XamarinBluetoothManager : BluetoothSubtypeManager
+    {
+        public XamarinBluetoothManager(IButtplugLogManager logManager) 
+            : base(logManager)
+        {
+
+        }
+
+        public override bool IsScanning()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StartScanning()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StopScanning()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
